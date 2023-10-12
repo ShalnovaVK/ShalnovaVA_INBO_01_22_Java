c0ed48608f9250c3d86c1bacaefd8ed8b8bd2b10
ShalnovaVK <147714738+ShalnovaVK@users.noreply.github.com>
2023-10-12 15:08:50 +0300

    Delete src directory

--- src/IWaitList.java
+++
@@ -1,9 +0,0 @@
-import java.util.Collection;
-
-public interface IWaitList<E> {
-   public void Add(E element);
-   public E Remove();
-   public boolean contains(E element);
-   public boolean containsAll(Collection<E> c);
-   public boolean isEmpty();
-}
